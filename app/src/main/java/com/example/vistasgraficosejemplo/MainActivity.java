c689a0aca2a29143f9f13b9499012dab6c219809
sgomera <silviagomera@gmail.com>
2019-06-14 12:46:37 +0200

    Fill and Stroke

--- app/src/main/java/com/example/vistasgraficosejemplo/MainActivity.java
+++ app/src/main/java/com/example/vistasgraficosejemplo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.vistasgraficosejemplo;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,11 +27,19 @@ public class MainActivity extends AppCompatActivity {
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             Paint miPincel = new Paint();
-            miPincel.setColor(getResources().getColor(android.R.color.holo_green_dark));
-            miPincel.setStrokeWidth(8);
-            miPincel.setStyle(Paint.Style.FILL);
 
+           // miPincel.setColor(getResources().getColor(android.R.color.holo_green_dark));
+            miPincel.setColor(Color.argb(200, 100,10,255));
+            miPincel.setStrokeWidth(8);
+            miPincel.setStyle(Paint.Style.STROKE);
             canvas.drawCircle(250, 500,200,miPincel);
+
+            //relleno
+            int miColor;
+            miColor = ContextCompat.getColor(getContext(), R.color.rellenoCirculo);
+            miPincel.setColor(miColor);
+            miPincel.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(250,500,200,miPincel);
         }
     }
 }
