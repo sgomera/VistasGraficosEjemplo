0c480ffe6b2e8fbfa0db35543ca92cb59c32a918
sgomera <silviagomera@gmail.com>
2019-06-14 13:03:04 +0200

    Fill and Stroke

--- app/src/main/java/com/example/vistasgraficosejemplo/MainActivity.java
+++ app/src/main/java/com/example/vistasgraficosejemplo/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
+            /*super.onDraw(canvas);
             Paint miPincel = new Paint();
 
            // miPincel.setColor(getResources().getColor(android.R.color.holo_green_dark));
@@ -39,7 +41,21 @@ public class MainActivity extends AppCompatActivity {
             miColor = ContextCompat.getColor(getContext(), R.color.rellenoCirculo);
             miPincel.setColor(miColor);
             miPincel.setStyle(Paint.Style.FILL);
-            canvas.drawCircle(250,500,200,miPincel);
+            canvas.drawCircle(250,500,200,miPincel);*/
+
+            Path miTrazo = new Path();
+            miTrazo.addCircle(350,350,200,Path.Direction.CCW);
+            Paint miPincel = new Paint();
+            miPincel.setColor(Color.RED);
+            miPincel.setStrokeWidth(8);
+            miPincel.setStyle(Paint.Style.STROKE);
+            canvas.drawPath(miTrazo, miPincel);
+
+            miPincel.setStrokeWidth(1);
+            miPincel.setStyle(Paint.Style.FILL);
+            miPincel.setTextSize(40);
+            miPincel.setTypeface(Typeface.SANS_SERIF);
+            canvas.drawTextOnPath("Curso Android Píldoras Informáticas",miTrazo, 40, 60, miPincel );
         }
     }
 }
